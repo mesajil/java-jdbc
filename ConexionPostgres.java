604eb2de1e395932962e7da48483a14baafd94d9
Luis Mesajil <lhmesajil@gmail.com>
2025-04-19 20:46:23 -0500

    Refactor ConexionPostgres to clean up commented code for job insertion and update

--- ConexionPostgres.java
+++ ConexionPostgres.java
@@ -1,17 +1,20 @@
 public class ConexionPostgres {
   public static void main(String[] args) {
-    // Insert new Job
-    // JobDAO.addJob(new Job(3, "Java Expert", "null", "Java Spring Boot Expert", "Perú", "3K - 5K
-    // $",
-    // "Microsoft", "-", "contact@microsoft.com", "+99 999 999 999"));
+    // Job job2 = createJob();
+
+    // Get Job
+    Job job = JobDAO.getJob(1);
+    // job.setType("Part-Time");
+    // JobDAO.updateJob(job);
 
     // Print Job
-    Job job = JobDAO.getJob(1);
-    job.setType("Part-Time");
-    JobDAO.updateJob(job);
-
     if (job != null) {
       System.out.println(job.toString());
     }
+  }
+
+  private static Job createJob() {
+    return new Job(3, "Java Expert", "null", "Java Spring Boot Expert", "Perú", "3K - 5K $",
+        "Microsoft", "-", "contact@microsoft.com", "+99 999 999 999");
   }
 }
